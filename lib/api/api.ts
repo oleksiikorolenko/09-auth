f925cffaf780f81d7ed9c513c0dcc478d7dfe815
Korolenko <korolenkoalexey84@gmail.com>
2025-08-23 14:12:13 +0300

    second commit

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -2,16 +2,11 @@ import axios from 'axios';
 
 
 
-const myKey = process.env.NEXT_PUBLIC_API_URL
-;
 
-if (myKey) {
-  axios.defaults.headers.common['Authorization'] = `Bearer ${myKey}`;
-} else {
-  console.warn('NEXT_PUBLIC_NOTEHUB_TOKEN is not defined.');
-}
-
-export const api = axios.create({
-  baseURL: 'https://notehub-api.goit.study',
+const api = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL + "/api",
   withCredentials: true,
 });
+
+
+export default api;
--- lib/api/clientApi.ts
+++ lib/api/clientApi.ts
@@ -1,9 +1,10 @@
-import axios from "axios";
+import api from "./api";
+
 import { Note, FetchNotesParams, RawFetchNotesResponse, FetchNotesResponse, NewNoteData } from "@/types/note";
 
 
 export const fetchNotes = async ({page = 1, perPage = 12, search = '', tag}: FetchNotesParams): Promise<FetchNotesResponse> => {
-    const response = await axios.get<RawFetchNotesResponse>('/notes', {
+    const response = await api.get<RawFetchNotesResponse>('/notes', {
         params: {
             page,
             perPage,
@@ -27,19 +28,19 @@ export const fetchNotes = async ({page = 1, perPage = 12, search = '', tag}: Fet
 
 
 export const fetchNoteById = async (id: string): Promise<Note> => {
-    const response = await axios.get<Note>(`/notes/${id}`);
+    const response = await api.get<Note>(`/notes/${id}`);
     return response.data;
 };
 
 
 export const createNote = async (note: NewNoteData): Promise<Note> => {
-    const response = await axios.post<Note>('/notes', note);
+    const response = await api.post<Note>('/notes', note);
 console.log('fetchNotes params:', response.data);
     return response.data;
 };
 
 export const deleteNote = async (id: string): Promise<Note> => {
-    const response = await axios.delete<Note>(`/notes/${id}`);
+    const response = await api.delete<Note>(`/notes/${id}`);
     return response.data;
 };
 
